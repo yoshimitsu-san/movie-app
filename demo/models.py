c162a436091740faed03a89292a123b2b5714a99
yoshimitsu-san <jjvtapps@gmail.com>
2019-08-04 14:45:41 +0200

    field options section

--- demo/models.py
+++ demo/models.py
@@ -2,4 +2,4 @@ from django.db import models
 
 # Create your models here.
 class Book(models.Model):
-    title = models.CharField(max_length=36)
+    title = models.CharField(blank=False, unique=True, max_length=36)
