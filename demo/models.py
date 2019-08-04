b26081c544a15c2ad846eaeedaf74df064989a18
yoshimitsu-san <jjvtapps@gmail.com>
2019-08-04 15:16:57 +0200

    field types section

--- demo/models.py
+++ demo/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 class Book(models.Model):
     title = models.CharField(blank=False, unique=True, max_length=36)
+    description = models.TextField(blank=True, max_length=256)
+    is_published = models.BooleanField(default=False)
+    published = models.DateField(blank=True, null=True, default=None)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=3)
+    cover = models.ImageField(blank=True, upload_to='covers/')
