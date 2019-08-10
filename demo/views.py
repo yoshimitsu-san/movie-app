b9aaec9d2bbd46cde04e9025fa134599a8a0c26d
yoshimitsu-san <jjvtapps@gmail.com>
2019-08-10 16:28:40 +0200

    Templates section

--- demo/views.py
+++ demo/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
 from .models import Book
 
 
-class Another(View):
-
-    books = Book.objects.all()
-
-    output = ''
-
-    for book in books:
-        output += f"{book.title} (Book ID {book.id}) was published on {book.published}<br>"
-
-    def get(self, request):
-        return HttpResponse(self.output)
-
-
 def first(request):
-    return HttpResponse('First message from views')
+    return render(request, 'first_temp.html')
