ec26cb159adff2bf306c6e12414e2a6ee4287dae
yoshimitsu-san <jjvtapps@gmail.com>
2019-08-11 18:16:06 +0200

    updated migration files

--- demo/migrations/0008_auto_20190811_1457.py
+++ demo/migrations/0008_auto_20190811_1457.py
@@ -5,6 +5,8 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
+    atomic = False
+
     dependencies = [
         ('demo', '0007_auto_20190811_1456'),
     ]
--- demo/migrations/0009_auto_20190811_1501.py
+++ demo/migrations/0009_auto_20190811_1501.py
@@ -5,6 +5,8 @@ from django.db import migrations
 
 class Migration(migrations.Migration):
 
+    atomic = False
+
     dependencies = [
         ('demo', '0008_auto_20190811_1457'),
     ]
